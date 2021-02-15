a59ea24949033182cec0947618728673c735e1b6
ethanpil <ethan@piliavin.com>
2021-02-15 15:44:40 -0800

    Update to match latest WP version

--- install.php
+++ install.php
@@ -21,6 +21,7 @@ function wp_install($blog_title, $user_name, $user_email, $public, $deprecated =
 
 	wp_check_mysql_version();
 	wp_cache_flush();
+  make_db_current_silent();
 	/* changes */
 	require_once PDODIR . 'schema.php';
 	make_db_sqlite();
@@ -32,47 +33,79 @@ function wp_install($blog_title, $user_name, $user_email, $public, $deprecated =
 	update_option('admin_email', $user_email);
 	update_option('blog_public', $public);
 
-	$guessurl = wp_guess_url();
+		// Freshness of site - in the future, this could get more specific about actions taken, perhaps.
+		update_option( 'fresh_site', 1 );
 
-	update_option('siteurl', $guessurl);
+		if ( $language ) {
+			update_option( 'WPLANG', $language );
+		}
 
-	if (!$public)
-		update_option('default_pingback_flag', 0);
+		$guessurl = wp_guess_url();
 
-	$user_id        = username_exists($user_name);
-	$user_password  = trim($user_password);
-	$email_password = false;
-	if (!$user_id && empty($user_password)) {
-		$user_password = wp_generate_password(12, false);
-		$message = __('<strong><em>Note that password</em></strong> carefully! It is a <em>random</em> password that was generated just for you.');
-		$user_id = wp_create_user($user_name, $user_password, $user_email);
-		update_user_option($user_id, 'default_password_nag', true, true);
-		$email_password = true;
-	} else if (!$user_id) {
-		$message = '<em>'.__('Your chosen password.').'</em>';
-		$user_id = wp_create_user($user_name, $user_password, $user_email);
-	}
+		update_option( 'siteurl', $guessurl );
 
-	$user = new WP_User($user_id);
-	$user->set_role('administrator');
+		// If not a public site, don't ping.
+		if ( ! $public ) {
+			update_option( 'default_pingback_flag', 0 );
+		}
 
-	wp_install_defaults($user_id);
+		/*
+		 * Create default user. If the user already exists, the user tables are
+		 * being shared among sites. Just set the role in that case.
+		 */
+		$user_id        = username_exists( $user_name );
+		$user_password  = trim( $user_password );
+		$email_password = false;
+		$user_created   = false;
 
-	flush_rewrite_rules();
+		if ( ! $user_id && empty( $user_password ) ) {
+			$user_password = wp_generate_password( 12, false );
+			$message       = __( '<strong><em>Note that password</em></strong> carefully! It is a <em>random</em> password that was generated just for you.' );
+			$user_id       = wp_create_user( $user_name, $user_password, $user_email );
+			update_user_option( $user_id, 'default_password_nag', true, true );
+			$email_password = true;
+			$user_created   = true;
+		} elseif ( ! $user_id ) {
+			// Password has been provided.
+			$message      = '<em>' . __( 'Your chosen password.' ) . '</em>';
+			$user_id      = wp_create_user( $user_name, $user_password, $user_email );
+			$user_created = true;
+		} else {
+			$message = __( 'User already exists. Password inherited.' );
+		}
 
-	wp_new_blog_notification($blog_title, $guessurl, $user_id, ($email_password ? $user_password : __('The password you chose during the install.')));
+		$user = new WP_User( $user_id );
+		$user->set_role( 'administrator' );
 
-	wp_cache_flush();
+		if ( $user_created ) {
+			$user->user_url = $guessurl;
+			wp_update_user( $user );
+		}
 
-	if (isset($_SERVER['SERVER_SOFTWARE']) && stripos($_SERVER['SERVER_SOFTWARE'], 'apache') !== false || isset($_SERVER['SERVER_SIGNATURE']) && stripos($_SERVER['SERVER_SIGNATURE'], 'apache') !== false) {
-		;// Your server is Apache. Nothing to do more.
-	} else {
-		$server_message = sprintf('Your webserver doesn\'t seem to be Apache. So the database directory access restriction by the .htaccess file may not function. We strongly recommend that you should restrict the access to the directory %s in some other way.', FQDBDIR);
-		echo '<div style="position: absolute; margin-top: 350px; width: 700px; border: .5px dashed rgb(0, 0, 0);"><p style="margin: 10px;">';
-		echo $server_message;
-		echo '</p></div>';
-	}
+		wp_install_defaults( $user_id );
 
-	return array('url' => $guessurl, 'user_id' => $user_id, 'password' => $user_password, 'password_message' => $message);
+		wp_install_maybe_enable_pretty_permalinks();
+
+		flush_rewrite_rules();
+
+		wp_new_blog_notification( $blog_title, $guessurl, $user_id, ( $email_password ? $user_password : __( 'The password you chose during installation.' ) ) );
+
+		wp_cache_flush();
+
+		/**
+		 * Fires after a site is fully installed.
+		 *
+		 * @since 3.9.0
+		 *
+		 * @param WP_User $user The site owner.
+		 */
+		do_action( 'wp_install', $user );
+
+		return array(
+			'url'              => $guessurl,
+			'user_id'          => $user_id,
+			'password'         => $user_password,
+			'password_message' => $message,
+		);
 }
 ?>
